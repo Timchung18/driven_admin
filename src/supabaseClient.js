ede01d7ac039233cd840c1399cef2047e9549287
Timothy <Timothy@Timothy-Cheung-MacBook-Pro.local>
2024-11-21 13:42:15 -0800

    using env file

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://lgucbcmcvzypyrhjkzaw.supabase.co'; // Replace with your Supabase API URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxndWNiY21jdnp5cHlyaGpremF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE5OTY3MDYsImV4cCI6MjA0NzU3MjcwNn0.RGmdeEcbi0vLLS-CUX85xkJRkfFAKQiRV7N6NGX5SVc'; // Replace with your Supabase Anon Key
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
